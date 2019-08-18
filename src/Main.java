12eb3165babb42c7b102bfdb64596aa9e5c9bb56
vikramlc <vikram.chawan121@gmail.com>
2019-08-18 22:11:59 +0530

    Sliding window: optimised

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,15 @@ public class Main {
 
     // This approach tries to keep i at the start of the longest substring while j at the end of the longest substring
     public static int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        while (i < n && j < n) {
-            // try to extend the range [i, j]
-            if (!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j - i);
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
             }
-            else {
-                set.remove(s.charAt(i++));
-            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
         }
         return ans;
     }

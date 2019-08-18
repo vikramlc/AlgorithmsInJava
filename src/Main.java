fc85d789e3cd71c07c2ee19450b817d00a71697b
vikramlc <vikram.chawan121@gmail.com>
2019-08-18 21:47:51 +0530

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,37 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {
-                5, 2, 4, 6, 1, 3
-        };
 
-        int[] b = {
-                7, 10, 8, 9, 1, 4
-        };
-
-        addBinary(a, b);
-
+        System.out.println("Length: " + lengthOfLongestSubstring("dvdf"));
     }
 
 
-    public static void addBinary(int[] a, int[] b) {
-
-        Integer[] c = new Integer[a.length];
-
-        for(int i=0; i<a.length; i++) {
-            c[i] = Integer.valueOf(a[i]+b[i]);
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (allUnique(s, i, j)) {
+                    ans = Math.max(ans, j - i);
+                }
+            }
         }
+        return ans;
+    }
 
-        for(Integer value: c) {
-            System.out.print(Integer.toBinaryString(value) + " ");
+    public static boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
         }
-
+        return true;
     }
 }
 

edcb29d89067d75bcc1895aefc6993239adcd875
vikramlc <vikram.chawan121@gmail.com>
2019-08-18 22:25:16 +0530

    Using sliding window: ASCII solution

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Length: " + lengthOfLongestSubstring("dvdf"));
+        System.out.println("Length: " + lengthOfLongestSubstring("aaddvdf"));
     }
 
     // This approach tries to keep i at the start of the longest substring while j at the end of the longest substring
     public static int lengthOfLongestSubstring(String s) {
         int n = s.length(), ans = 0;
-        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        int[] index = new int[128]; // current index of character
         // try to extend the range [i, j]
         for (int j = 0, i = 0; j < n; j++) {
-            if (map.containsKey(s.charAt(j))) {
-                i = Math.max(map.get(s.charAt(j)), i);
-            }
+            i = Math.max(index[s.charAt(j)], i);
             ans = Math.max(ans, j - i + 1);
-            map.put(s.charAt(j), j + 1);
+            index[s.charAt(j)] = j + 1;
         }
         return ans;
     }

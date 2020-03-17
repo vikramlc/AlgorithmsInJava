6018e924c8242cacba4c25cffef9009b431e7339
1994 <vikram.chawan@solvezy.com>
2020-03-17 20:33:52 +0530

    Factorial - recursion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Factorial of 7 is " + factorial(7));
+    }
 
+    private static int factorial(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n-1);
     }
 }

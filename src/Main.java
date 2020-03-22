2e1c792122854fdbff06e1a890f8c87069bf5ca3
1994 <vikram.chawan@solvezy.com>
2020-03-22 20:50:16 +0530

    Fibonacci - recursion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String number = br.readLine();
+        System.out.println("Fibonacci sum of 6 is " + fibonacci(Integer.valueOf(number)));
+    }
 
+    private static long fibonacci(long n) {
+        long sum = 0;
+        // Recursion
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+
+        /*if(n == 0 || n == 1) {
+            return n;
+        }
+
+        return (n-1) + (n-2);*/
     }
 }

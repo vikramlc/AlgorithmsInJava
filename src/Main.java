17e4605841818890fdca8e8428d263b96daa82d2
1994 <vikram.chawan@solvezy.com>
2020-04-04 12:24:46 +0530

    sort - selection sort

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
 
-    public static void main(String[] args) {
+    private static void selectionSort(int[] nums) {
+        for (int i=0; i<nums.length-1; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
 
+        System.out.println("Selection sort: ");
+        for(int num: nums) {
+            System.out.print(num + " -> ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {20, 10, 80, 30, 40, 5, 19, 1, 81};
+        selectionSort(nums);
     }
 }
